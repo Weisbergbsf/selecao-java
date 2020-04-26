dc34cc0c3549e528dc5cead7a378ae7454651835
Weisberg Barros <weisbergbsf@gmail.com>
2020-04-26 15:34:33 -0300

    Criado exception customizadas

---
+++ backend/src/main/java/com/indra/api/resource/exception/StandardError.java
@@ -0,0 +1,26 @@
+package com.indra.api.resource.exception;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class StandardError implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long timestamp;
+	private Integer status;
+	private String error;
+	private String message;
+	private String path;
+
+}
