5270b365b01575452ce949479a552cfa1d43a339
Weisberg Barros <weisbergbsf@gmail.com>
2020-04-26 15:34:59 -0300

    Criado classe para resposta padrão

---
+++ backend/src/main/java/com/indra/api/resource/payload/ApiResponse.java
@@ -0,0 +1,21 @@
+package com.indra.api.resource.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class ApiResponse {
+	
+	private Boolean success;
+    private Object value;
+
+}
