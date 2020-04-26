77f2265c11a2d3fe43c0fc9978ef79211c46f973
Weisberg Barros <weisbergbsf@gmail.com>
2020-04-26 15:19:45 -0300

    Entidade Estado e Repositorio criado

---
+++ backend/src/main/java/com/indra/api/model/Estado.java
@@ -0,0 +1,33 @@
+package com.indra.api.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Entity
+@Table(name = "estados")
+public class Estado {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+
+	private String nome;
+
+
+}
