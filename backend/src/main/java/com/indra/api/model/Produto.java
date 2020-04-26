13266c828e6ca703f8faf99a7a43b26f7195f90a
Weisberg Barros <weisbergbsf@gmail.com>
2020-04-26 15:20:39 -0300

    Entidade Produto e Repositorio criado

---
+++ backend/src/main/java/com/indra/api/model/Produto.java
@@ -0,0 +1,47 @@
+package com.indra.api.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Entity
+@Table(name = "produtos")
+public class Produto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+
+	@Column(nullable = false, length = 200)
+	private String nome;
+
+	@Column(name = "unidade_medida", nullable = false)
+	private String unidadeMedida;
+
+	@ManyToOne
+	@JoinColumn(name = "bandeira")
+	private Bandeira bandeira;
+
+}
